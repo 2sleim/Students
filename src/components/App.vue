5ea54fbf28862ce1aae28d7288890eb7e2eeff65
2sleim <95756439+2sleim@users.noreply.github.com>
2023-01-27 12:55:58 +0100

    Students

---
+++ src/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main" :class="{ white: isWhite, dark: !isWhite }">
+    <div class="content">
+      <router-view></router-view>
+      <button @click="changeTheme" id="changeThemeButton">Змінити тему</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      isWhite: "",
+    };
+  },
+  mounted() {
+    this.isWhite = this.$store.getters.getTheme;
+  },
+  methods: {
+    changeTheme() {
+      this.isWhite = !this.isWhite;
+      this.$store.commit("changeTheme", this.isWhite);
+    },
+  },
+};
+</script>
---
+++ src/components/StudentsTable.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-link :to="'/student-info/' + item._id">
+    <td>{{ item.name }}</td>
+  </router-link>
+  <td><input type="checkbox" v-model="item.isDonePr" /></td>
+  <td>{{ item.group }}</td>
+  <td><a href="#" @click="$emit('delete', item._id)">Delete</a></td>
+</template>
+
+<script>
+export default {
+  name: "StudentsTable",
+  props: {
+    item: {
+      type: Object,
+      require: true,
+    },
+  },
+};
+</script>
